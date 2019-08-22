13f633a4bc8c0befdee3745e000b8089a8c2d0a4
Aashish Sharma <aashish2057@gmail.com>
2019-08-22 09:10:59 -0500

    File setup

---
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 08:58:58 2019
+
+@author: aashi
+"""
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
